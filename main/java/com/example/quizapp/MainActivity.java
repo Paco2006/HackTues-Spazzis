45c5d8f10e03b5380358b924225b79d4f62e6f5a
GogoMilioni <101291138+GogoMilioni@users.noreply.github.com>
2022-03-13 05:54:06 +0200

    Add files via upload

---
+++ main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,82 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String selectedDifficultyLevel = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final LinearLayout easy = findViewById(R.id.easyLayout);
+        final LinearLayout medium = findViewById(R.id.mediumLayout);
+        final LinearLayout hard = findViewById(R.id.hardLayout);
+
+        final Button topic = findViewById(R.id.gtopBtn);
+
+
+        easy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedDifficultyLevel = "easy";
+
+                easy.setBackgroundResource(R.drawable.round_back_white_stroke);
+
+                medium.setBackgroundResource(R.drawable.difficulty);
+                hard.setBackgroundResource(R.drawable.difficulty);
+            }
+        });
+
+        medium.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedDifficultyLevel = "medium";
+
+                medium.setBackgroundResource(R.drawable.round_back_white_stroke);
+
+                easy.setBackgroundResource(R.drawable.difficulty);
+                hard.setBackgroundResource(R.drawable.difficulty);
+            }
+        });
+
+        hard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                selectedDifficultyLevel = "hard";
+
+                hard.setBackgroundResource(R.drawable.round_back_white_stroke);
+
+                easy.setBackgroundResource(R.drawable.difficulty);
+                medium.setBackgroundResource(R.drawable.difficulty);
+            }
+        });
+
+        topic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(selectedDifficultyLevel.isEmpty()){
+                    Toast.makeText(MainActivity.this, "Please select a Difficulty level", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Intent intent = new Intent(MainActivity.this, QuizActivity.class);
+                    intent.putExtra("selectedDiff", selectedDifficultyLevel);
+                    startActivity(intent);
+                }
+            }
+        });
+    }
+}
